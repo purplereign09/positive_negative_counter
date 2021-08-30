4e8dc993ba590b3fc4bd064eda862349f8544f57
Patty Baraibar <pattybaraibar@msn.com>
2021-08-30 01:29:38 -0500

    added function

---
+++ script.js
@@ -0,0 +1,15 @@
+//set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+// console.log(btns);
+
+//looping through buttons
+btns.forEach(function (btn) {
+    btn.addEventListener('click',function(e) {
+        console.log(e.currentTarget.classList);
+    });
+});
