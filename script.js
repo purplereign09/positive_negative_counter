0c6647691fd08573a77f106edb8ab43a581319e2
Patty Baraibar <pattybaraibar@msn.com>
2021-08-30 02:14:57 -0500

    added javascript

--- script.js
+++ script.js
@@ -9,7 +9,27 @@ const btns = document.querySelectorAll(".btn");
 
 //looping through buttons
 btns.forEach(function (btn) {
+    //using the event object (e)//getting the target and list
     btn.addEventListener('click',function(e) {
-        console.log(e.currentTarget.classList);
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')) {
+            count--;
+        }
+        else if(styles.contains('increase')) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        else if(count < 0) {
+            value.style.color = "red";
+        }
+        else {
+            value.style.color = "black";
+        }
+        value.textContent = count;
     });
 });
